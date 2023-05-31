247e2c84c06d43bbde7e23058a236896a89e52ec
EternalWind <eternalwind@live.cn>
2023-05-31 20:58:17 +0800

    Added maven release plugin

---
+++ src/test/java/io/eternalwind/jmsexample/ConsumerTest.java
@@ -0,0 +1,13 @@
+package io.eternalwind.jmsexample;
+
+import org.junit.jupiter.api.Test;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+class ConsumerTest {
+    @Test
+    void test() {
+        log.info("Test is running!");
+    }
+}
